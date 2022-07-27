cbc5b12d35ad0ea65cbe985ce1616944ac96afef
Attila Wiedemann <attila.wiedemann@gmail.com>
2022-07-27 19:14:45 +0200

    Added initial files

---
+++ src/main/java/com/attila/activity/ActivityApplication.java
@@ -0,0 +1,14 @@
+package com.attila.activity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityApplication.class, args);
+	}
+
+}
